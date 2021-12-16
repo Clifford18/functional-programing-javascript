ee212c63cfa4f034cfb175f1de7ad4de319dd60e
Clifford18 <cliffordjohn.cj@gmail.com>
2021-12-16 14:45:14 +0300

    update app.js with filters

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log ("tet")
+const {filterDogs,forLoopDogs, alterFilterDogs} = require("./filter")
+
+console.log ("test")
+
+console.log ("@Dogs filter", JSON.stringify(filterDogs, null, 2) )
+
+console.log ("@Dogs forloop", JSON.stringify(forLoopDogs, null, 2) )
+
+console.log ("@Dogs alternate ", JSON.stringify(alterFilterDogs, null, 2) )
