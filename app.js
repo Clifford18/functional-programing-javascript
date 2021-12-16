4fec4c04df1fa70f477ca4879e38da2423affc6c
Clifford18 <cliffordjohn.cj@gmail.com>
2021-12-16 15:28:37 +0300

    update app.js file

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const {filterDogs,forLoopDogs, alterFilterDogs} = require("./filter")
-
+const {forNames,mapNames, mapNamesArrowFunc, mapNamesArrowFuncImplicitReturn} =require("./map")
 console.log ("test")
 
+//filter
 console.log ("@Dogs filter", JSON.stringify(filterDogs, null, 2) )
-
 console.log ("@Dogs forloop", JSON.stringify(forLoopDogs, null, 2) )
-
 console.log ("@Dogs alternate ", JSON.stringify(alterFilterDogs, null, 2) )
+//maps
+console.log ("@Names Forloop ", JSON.stringify(forNames, null, 2) )
+console.log ("@Names Maps ", JSON.stringify(mapNames, null, 2) )
+console.log ("@Names MapsArrowFunction ", JSON.stringify(mapNamesArrowFunc, null, 2) )
+console.log ("@Names MapsArrowFunctionImplicitReturn  ", JSON.stringify(mapNamesArrowFuncImplicitReturn, null, 2) )
