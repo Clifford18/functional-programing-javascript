e66e5474557d88909177e076e2f69409b1f76f82
Clifford18 <cliffordjohn.cj@gmail.com>
2021-12-17 10:50:18 +0300

    add reduce.js file

---
+++ reduce.js
@@ -0,0 +1,22 @@
+var orders =[
+    {amount: 250},
+    {amount: 400},
+    {amount: 100},
+    {amount: 325}
+]
+//reduce
+var reduceTotalAmount = orders.reduce(function (sum,order) {
+    console.log("Hello",sum,order)
+    return sum +order.amount
+},0)
+
+//reduce arrowfunct
+var reduceArrowFuncTotalAmount = orders.reduce((sum,order) => sum +order.amount,0)
+
+//for loop
+var forTotalAmount = 0
+for (var i =0; i<orders.length;i++){
+    forTotalAmount +=orders[i].amount
+}
+
+module.exports = {forTotalAmount,reduceTotalAmount,reduceArrowFuncTotalAmount}
