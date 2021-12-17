85d485c81521c1d038d03c5b5e58753b667de6ef
Clifford18 <cliffordjohn.cj@gmail.com>
2021-12-17 13:34:43 +0300

    add and update closures.js file

---
+++ closures.js
@@ -0,0 +1,17 @@
+var me = 'John Clifford'
+function greetMe() {
+    console.log('Hello, ' +me+ '!')
+}
+
+module.exports = {greetMe}
+
+function sendRequest() {
+    var requestID = '123'
+    $.ajax({
+        url: '/myUrl',
+        success: function(response) {
+            alert('Request ' + requestID + ' returned')
+        }
+    });
+}
+//resource https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
