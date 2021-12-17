707c2cf60188c34fde6f847dd9eb547dd04a0543
Clifford18 <cliffordjohn.cj@gmail.com>
2021-12-17 10:51:44 +0300

    add app.js file for basic reduce

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const {filterDogs,forLoopDogs, alterFilterDogs} = require("./filter")
 const {forNames,mapNames, mapNamesArrowFunc, mapNamesArrowFuncImplicitReturn} =require("./map")
+const {forTotalAmount,reduceTotalAmount,reduceArrowFuncTotalAmount} = require("./reduce")
 console.log ("test")
 
 //filter
 console.log ("@Dogs filter", JSON.stringify(filterDogs, null, 2) )
 console.log ("@Dogs forloop", JSON.stringify(forLoopDogs, null, 2) )
 console.log ("@Dogs alternate ", JSON.stringify(alterFilterDogs, null, 2) )
+
 //maps
 console.log ("@Names Forloop ", JSON.stringify(forNames, null, 2) )
 console.log ("@Names Maps ", JSON.stringify(mapNames, null, 2) )
 console.log ("@Names MapsArrowFunction ", JSON.stringify(mapNamesArrowFunc, null, 2) )
 console.log ("@Names MapsArrowFunctionImplicitReturn  ", JSON.stringify(mapNamesArrowFuncImplicitReturn, null, 2) )
+
+//reduce
+console.log ("@totalamount forTotalAmount  =", JSON.stringify(forTotalAmount, null, 2) )
+console.log ("@totalamount reduceTotalAmount  =", JSON.stringify(reduceTotalAmount, null, 2) )
+console.log ("@totalamount reduceArrowFuncTotalAmount  =", JSON.stringify(reduceArrowFuncTotalAmount, null, 2) )
