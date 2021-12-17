c8270c637f00e24819012f0eb7f06185431c8688
Clifford18 <cliffordjohn.cj@gmail.com>
2021-12-17 15:17:59 +0300

    update app.js with curry

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const {filterDogs,forLoopDogs, alterFilterDogs} = require("./filter")
 const {forNames,mapNames, mapNamesArrowFunc, mapNamesArrowFuncImplicitReturn} =require("./map")
 const {forTotalAmount,reduceTotalAmount,reduceArrowFuncTotalAmount} = require("./reduce")
 const {greetMe} = require("./closures");
+const {notCarryDragon, carryDragon,carry_Dragon, lightningDragons, lightningDragonsCurry} =require("./curry")
+
 console.log ("test")
 
 //filter
@@ -24,3 +26,12 @@ console.log ("@totalamount reduceArrowFuncTotalAmount  =", JSON.stringify(reduce
 
 //closure
 greetMe()
+
+//carry
+console.log (notCarryDragon('Fluffykins','tiny ','lightining'))
+console.log (carryDragon('Fluffykins')('big ')('fire'))
+console.log (carry_Dragon('Fluffykins')('tiny ')('fire'))
+console.log("@lightining :",JSON.stringify(lightningDragons,null,2))
+console.log("@lightining with curry :",JSON.stringify(lightningDragonsCurry,null,2))
+
+
