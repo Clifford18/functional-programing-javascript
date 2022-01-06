ef2d400c75b35999f6f8b908d688a96435dbfa2f
Clifford18 <cliffordjohn.cj@gmail.com>
2022-01-06 13:27:17 +0300

    update closures.js file

--- closures.js
+++ closures.js
@@ -1,17 +1,19 @@
-var me = 'John Clifford'
+var me = 'John Clifford';
+
 function greetMe() {
-    console.log('Hello, ' +me+ '!')
+	console.log('Hello, ' + me + '!');
 }
 
-module.exports = {greetMe}
+module.exports = {greetMe};
 
 function sendRequest() {
-    var requestID = '123'
-    $.ajax({
-        url: '/myUrl',
-        success: function(response) {
-            alert('Request ' + requestID + ' returned')
-        }
-    });
+	var requestID = '123';
+	$.ajax({
+		url: '/myUrl',
+		success: function (response) {
+			alert('Request ' + requestID + ' returned');
+		}
+	});
 }
+
 //resource https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
